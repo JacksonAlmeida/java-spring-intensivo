e3c59b3b157d951740ef8bf1700f8632072a0286
Jackson Rodrigues <jacksonrodrigues@fitec.org.br>
2023-07-14 22:07:43 -0300

    first commit

---
+++ src/main/java/com/sunflower/javaspringintensivo/JavaSpringIntensivoApplication.java
@@ -0,0 +1,13 @@
+package com.sunflower.javaspringintensivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringIntensivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringIntensivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunflower/javaspringintensivo/JavaSpringIntensivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunflower.javaspringintensivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringIntensivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
