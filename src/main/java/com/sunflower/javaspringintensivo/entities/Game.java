f6099ae316ab0ccb0ae46df8daa3ebe01e979d41
Jackson Rodrigues <jacksonnalmeida30@gmail.com>
2023-07-16 15:29:58 -0300

    Game seed

--- src/main/java/com/sunflower/javaspringintensivo/entities/Game.java
+++ src/main/java/com/sunflower/javaspringintensivo/entities/Game.java
@@ -22,21 +22,25 @@ public class Game {
 	private Integer year;
 	private String genre;
 	private String platform;
+	private Double score;
 	private String imgUrl;
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 	
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
 		this.platform = platform;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -71,6 +75,12 @@ public class Game {
 	}
 	public void setPlatform(String platform) {
 		this.platform = platform;
+	}
+	public Double getScore() {
+		return score;
+	}
+	public void setScore(Double score) {
+		this.score = score;
 	}
 	public String getImgUrl() {
 		return imgUrl;
